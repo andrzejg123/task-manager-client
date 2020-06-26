1fa4d3c4bb491624766869f2374e5d86213a903d
Andrzej <andrgre405@student.polsl.pl>
2020-06-26 10:15:44 +0200

    minor updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {User} from './_models/user';
 import {Router} from '@angular/router';
 import {AuthenticationService} from './_services/authentication.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   currentUser: User;
+  userSub: Subscription;
 
   constructor(
     private router: Router,
     public authenticationService: AuthenticationService
   ) {
-    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+    this.userSub = this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
 
   }
 
   logout() {
     this.authenticationService.logout();
     this.router.navigate(['/login']);
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
 }
